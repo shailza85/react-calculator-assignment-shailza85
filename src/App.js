ec89b094da13c36553fb46e97d2aa346bc460d4f
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-27 17:10:22 -0600

    Form created for calculator.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+  constructor (props)
+  {
+    super(props);
+    this.state = {  //this.state here used for reading values not writing.
+      input1: "", //Keep track of our new value.
+      input2: ""//Keep track of our new value.
+
+    };
+
+  }
+ 
+  inputValue1=(event)=> 
+  {
+     const inputVal1 = {
+      value:this.state.input1
+    };
+
+   console.log(inputVal1);
+   
+   this.setState ({  
+    input1: ""
+  });
+   
+  };   
+  
+   inputValue2=(event)=> 
+  {
+  
+
+    const inputVal2 = {
+      value:this.state.input2
+    };
+   console.log(inputVal2);
+
+   this.setState ({  
+    input2: ""
+  });
+  }; 
+  
+  render()
+  {
+    return(
+
+      <>
+      <h1>React To-Do App</h1>
+      <form>
+        <label htmlFor="input1">
+          Input 1:
+          <input type="text" name ="input1" 
+          id="newToDo" value={this.state.input1}
+          onChange={ event =>this.inputValue1('input1', event.target.value)}
+           />
+        </label>
+        <label htmlFor="select1">
+          Operation:
+          <select type="text" name ="select1" 
+          id="select1">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select>
+        </label>
+
+        <label htmlFor="input2">
+          Input 2:
+          <input type="text" name ="input2" 
+          id="newToDo"
+          value={this.state.input2}  onChange={ event =>this.inputValue1('input2', event.target.value)} />
+        </label>
+        <input type="submit" value="Calculate"/>
+      </form>
+      
+     
+      </>
+    );
+
+  }
 }
-
 export default App;
+
